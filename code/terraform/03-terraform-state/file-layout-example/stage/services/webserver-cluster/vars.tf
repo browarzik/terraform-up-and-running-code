5dd89e04159245509ae5bb03b5755b3370afc7b4
Dirk Browarzik <Dirk.Browarzik@trustedshops.de>
2019-01-04 16:07:36 +0100

    set version number

--- code/terraform/03-terraform-state/file-layout-example/stage/services/webserver-cluster/vars.tf
+++ code/terraform/03-terraform-state/file-layout-example/stage/services/webserver-cluster/vars.tf
@@ -5,8 +5,10 @@ variable "server_port" {
 
 variable "db_remote_state_bucket" {
   description = "The name of the S3 bucket used for the database's remote state storage"
+  default     = "trusted-dev-extdbr"
 }
 
 variable "db_remote_state_key" {
   description = "The name of the key in the S3 bucket used for the database's remote state storage"
+  default     = "db"
 }
